b8b3baab81931635ccb8908b5a076b76bcb0f2fc
sarafernandezab <saraf.abalde@gmail.com>
2016-01-08 22:23:09 +0100

    GREEN
    2nd commit: VowelCounter implemented

--- src/LetterCounter.java
+++ src/LetterCounter.java
@@ -2,7 +2,17 @@
 public class LetterCounter {
 
 	public int VocalCounter(String string) {
-		return 0;
+	    int count = 0; // start the count at zero
+	    // change the string to lowercase
+	    string = string.toLowerCase();
+	  
+	    for (int i = 0; i < string.length(); i++) {
+	        char c = string.charAt(i);
+	        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
+	            count++;
+	        }
+	    }
+	    return count;
 	}
 
 }
