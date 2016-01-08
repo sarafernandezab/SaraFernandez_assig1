5cf74409575cd750aaa7c065efcbe059b830a340
sarafernandezab <saraf.abalde@gmail.com>
2016-01-09 00:05:22 +0100

    RED
    7th commit: Testing with two and three works

--- src/testWordsChecker.java
+++ src/testWordsChecker.java
@@ -4,13 +4,14 @@ public class testWordsChecker extends TestCase{
 
 	public void testVowelCounter(){
 		LetterCounter l = new LetterCounter();
-		int result = l.VowelCounter("testingVow");
+		int result = l.VowelCounter("testingVow twowords");
         assertEquals(3, result);
 	}
 	
 	public void testConsonantCounter(){
 		LetterCounter l = new LetterCounter();
-		int result = l.ConsonantCounter("testingCons");
+		int result = l.ConsonantCounter("testingCons three words");
 		assertEquals(8, result);
 	}
+
 }
