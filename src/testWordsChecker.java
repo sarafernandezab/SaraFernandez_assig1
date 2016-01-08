8b65ba9614ac57f438a44d1adeda05f63ae73e5a
sarafernandezab <saraf.abalde@gmail.com>
2016-01-09 00:56:00 +0100

    RED
    12th commit: Testing strings with symbols

--- src/testWordsChecker.java
+++ src/testWordsChecker.java
@@ -4,19 +4,19 @@ public class testWordsChecker extends TestCase{
 
 	public void testVowelCounter(){
 		WordsChecker w = new WordsChecker();
-		int result = w.VowelCounter("testingVow twowords");
+		int result = w.VowelCounter("testingVow, & twowords");
         assertEquals(5, result);
 	}
 	
 	public void testConsonantCounter(){
 		WordsChecker w = new WordsChecker();
-		int result = w.ConsonantCounter("testingCons three words");
+		int result = w.ConsonantCounter("testingCons $three words");
 		assertEquals(15, result);
 	}
 
 	public void testPalindromeChecker(){
 		WordsChecker w = new WordsChecker();
-		boolean result = w.PalindromeChecker("Amore Roma");
+		boolean result = w.PalindromeChecker("Amore, Roma.");
 		assertEquals(true, result);
 	}
 }
